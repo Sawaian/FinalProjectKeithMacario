02767438c95b8927a30d1c4a8bad5fafa7974229
Keith Macario <30133974+Sawaian@users.noreply.github.com>
2025-07-30 04:04:51 -0700

    Player_Character

--- Assets/Scripts/Player/Player_Control.cs
+++ Assets/Scripts/Player/Player_Control.cs
@@ -1,16 +1,68 @@
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Control : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Vector2 movement;
+  public Animator animator;
+
+    Rigidbody rigidBody;
+    [SerializeField] float moveSpeed = 5f;
+
+
+    void Awake()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+
+        if (animator == null)
+        {
+            Debug.LogWarning("Animator not found!");
+        }
+        else
+        {
+            Debug.Log("Animator assigned automatically.");
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
+        HandleMovement();
         
+        if (animator == null)
+{
+    Debug.LogWarning("Animator is NOT assigned!");
+}
+else
+{
+    Debug.Log("Animator IS assigned.");
+}
+ 
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+        Debug.Log(movement);
+    }
+
+    void HandleMovement()
+    {
+        Vector3 currenPosition = rigidBody.position;
+        Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y);
+        //Vector 1 + Vector 2 * a scalar. 
+        Vector3 newPosition = currenPosition + moveDirection * (moveSpeed * Time.fixedDeltaTime);
+        rigidBody.MovePosition(newPosition);
+        Debug.Log("Triggered");
+
+        float speed = moveDirection.magnitude;
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", speed);
+        }
+    
     }
 }
