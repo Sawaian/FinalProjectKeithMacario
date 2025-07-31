eefd25ca87d4ba2da86336809797230e07a9738c
Keith Macario <30133974+Sawaian@users.noreply.github.com>
2025-07-30 20:22:43 -0700

    Eight directional movement added. Decreased moveSpeed. Increased RotationSpeed. Made it feel snappier.

--- Assets/Scripts/Player/Player_Control.cs
+++ Assets/Scripts/Player/Player_Control.cs
@@ -1,17 +1,19 @@
 
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class Player_Control : MonoBehaviour
 {
     Vector2 movement;
-  public Animator animator;
+    public Animator animator;
 
     Rigidbody rigidBody;
     CapsuleCollider capsuleCollider;
-    [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float rotationSpeed = 2f;
-
+    [SerializeField] float moveSpeed = 2.94f;
+    [SerializeField] float rotationSpeed = 7.33f;
+    private Quaternion targetRotation;
+    private bool isMoving;
 
     void Awake()
     {
@@ -33,16 +35,17 @@ public class Player_Control : MonoBehaviour
     void FixedUpdate()
     {
         HandleMovement();
-        
+
         if (animator == null)
-{
-    Debug.LogWarning("Animator is NOT assigned!");
-}
-else
-{
-    Debug.Log("Animator IS assigned.");
-}
- 
+        {
+            Debug.LogWarning("Animator is NOT assigned!");
+        }
+        else
+        {
+            Debug.Log("Animator IS assigned.");
+        }
+
+
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -51,12 +54,55 @@ else
         Debug.Log(movement);
     }
 
-    public void Rotate()
+  
+    
+    public void HandleRotate(Vector3 moveDirection)
     {
-        
+        if (isMoving)
+        {
+             targetRotation = Quaternion.LookRotation(moveDirection);
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            targetRotation,
+            rotationSpeed * Time.fixedDeltaTime
+        );
+        }
     }
 
-   private bool IsGrounded()
+
+
+    void HandleMovement()
+    {
+        //Previously I passed newPosition. That was an error. It must be moveDirection. Where
+        //The character will go versus where it will be at upon completion. 
+
+        Vector3 currenPosition = rigidBody.position;
+        Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y);
+
+        //Vector 1 + Vector 2 * a scalar. 
+        Vector3 newPosition = currenPosition + moveDirection * (moveSpeed * Time.fixedDeltaTime);
+        rigidBody.MovePosition(newPosition);
+
+        if (newPosition != currenPosition)
+        {
+            isMoving = true;
+        }
+        else {
+            isMoving = false;
+        }
+
+        HandleRotate(moveDirection);
+
+
+        float speed = moveDirection.magnitude;
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", speed);
+        };
+
+    }
+    
+      private bool IsGrounded()
     {
         float extraAtBottomCapsuleHeight = 0.15f; // Buffer distance below the collider to avoid missing the ground.
         float groundRadius = 0.3f; //Radius of the sphere used in the checks.
@@ -84,21 +130,4 @@ else
         return isHit;
     }
 
-   
-    void HandleMovement()
-    {
-        Vector3 currenPosition = rigidBody.position;
-        Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y);
-        //Vector 1 + Vector 2 * a scalar. 
-        Vector3 newPosition = currenPosition + moveDirection * (moveSpeed * Time.fixedDeltaTime);
-        rigidBody.MovePosition(newPosition);
-        Debug.Log("Triggered");
-
-        float speed = moveDirection.magnitude;
-        if (animator != null)
-        {
-            animator.SetFloat("Speed", speed);
-        }
-    
-    }
 }
