ae8c14705c6c767d6be8a911d33e39c0bee1a2ed
Keith Macario <30133974+Sawaian@users.noreply.github.com>
2025-08-04 21:59:30 -0700

    Item script added along with ItemController. Character is now unable to turn appropiately.

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item")]
+[CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
 public class Item : ScriptableObject
 {
-    
+    public int id;
+    public string itemName;
+    public int value;
+    public Sprite icon;
 }
---
+++ Assets/Scripts/Items/ItemController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public Item item;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
